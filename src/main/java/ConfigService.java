6c6cceb2afe1af9120359e9b5739a528e1d6ceb1
tpalaschak <tpalaschak@umass.edu>
2019-03-01 11:25:36 -0500

    Added empty methods in ConfigService for reading/creating config files

--- src/main/java/ConfigService.java
+++ src/main/java/ConfigService.java
@@ -1,8 +1,18 @@
+import java.util.List;
+import java.util.Properties;
+
 public class ConfigService {
     private static final Integer INVENTORY_COUNT = 10;
 
-    public static Integer getInventoryCount(){
+    public static Integer getInventoryCount() {
         return INVENTORY_COUNT;
     }
-}
 
+    public Properties ipConfig() {
+        Properties prop = new Properties();
+        return prop;
+    }
+
+    public void getNeighbors(){
+    }
+}
